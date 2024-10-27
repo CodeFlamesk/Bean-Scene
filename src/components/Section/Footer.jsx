fa94b037f8155d5d90d82692da4f830fa5ec09f1
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-10-27 16:08:59 +0100

    ready label icon

--- src/components/Section/Footer.jsx
+++ src/components/Section/Footer.jsx
@@ -1,20 +1,9 @@
 import Container from "@components/Container";
-
-
 import FooterBackground from "../img/Footer/FooterBackground.webp"
 import FacebookIcon from "./img/Facebook";
 import InstagramIcon from "./img/Instagram";
 import YouTubeIcon from "./img/YouTube";
 import TwitterIcon from "./img/Twitter";
-
-
-
-
-
-
-
-
-
 const Footer = () => {
     return (
         <footer className='flex  bg-cover bg-center bg-no-repeat relative overflow-hidden' style={{ backgroundImage: `url(${FooterBackground})`, }}>
@@ -24,21 +13,21 @@ const Footer = () => {
                     <p className="clicker-fonts text-[54px] text-white">Bean Scene</p>
                     <p className="text-sm leading-[22px] text-white ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <div className="flex  gap-6 pt-4">
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="hover:text-facebook w-6 h-6 hover:scale-125 duration-300">
+                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="hover:text-facebook w-6 h-6 hover:scale-125 duration-300">
                             <FacebookIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="hover:text-instagram w-6 h-6 hover:scale-125 duration-300 ">
+                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="hover:text-instagram w-6 h-6 hover:scale-125 duration-300">
                             <InstagramIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="hover:text-red w-6 h-6 hover:scale-125 duration-300">
+                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="hover:text-red w-6 h-6 hover:scale-125 duration-300">
                             <YouTubeIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="hover:text-twitter w-6 h-6 hover:scale-125 duration-300 ">
+                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="hover:text-twitter w-6 h-6 hover:scale-125 duration-300">
                             <TwitterIcon />
                         </a>
                     </div>
                 </div>
-                {/* ссилки */}
+
                 <div className="flex flex-col lg:flex-row lg:gap-[66px] gap-y-10">
                     <div className="flex flex-col sm:flex-row lg:flex-col  gap-y-4 gap-x-8 lg:gap-0 bottom-anim">
                         <p className="text-[26px] font-bold lg:pb-[56px] w-[130px]">About</p>
