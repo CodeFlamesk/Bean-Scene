faeb7bdd5dbd91d50becb59bd685e2b8e8370ce5
Volodymyr Kravets <150174426+CodeFlamesk@users.noreply.github.com>
2025-04-15 17:49:22 +0200

    Update Footer.jsx

--- src/components/Section/Footer.jsx
+++ src/components/Section/Footer.jsx
@@ -13,16 +13,16 @@ const Footer = () => {
                     <p className="clicker-fonts text-[54px] text-white">Bean Scene</p>
                     <p className="text-sm leading-[22px] text-white ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <div className="flex  gap-6 pt-4">
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="hover:text-facebook w-6 h-6 hover:scale-125 duration-300">
+                        <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="hover:text-facebook w-6 h-6 hover:scale-125 duration-300">
                             <FacebookIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="hover:text-instagram w-6 h-6 hover:scale-125 duration-300">
+                        <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="hover:text-instagram w-6 h-6 hover:scale-125 duration-300">
                             <InstagramIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="hover:text-red w-6 h-6 hover:scale-125 duration-300">
+                        <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="hover:text-red w-6 h-6 hover:scale-125 duration-300">
                             <YouTubeIcon />
                         </a>
-                        <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="hover:text-twitter w-6 h-6 hover:scale-125 duration-300">
+                        <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="hover:text-twitter w-6 h-6 hover:scale-125 duration-300">
                             <TwitterIcon />
                         </a>
                     </div>
@@ -32,28 +32,28 @@ const Footer = () => {
                     <div className="flex flex-col sm:flex-row lg:flex-col  gap-y-4 gap-x-8 lg:gap-0 bottom-anim">
                         <p className="text-[26px] font-bold lg:pb-[56px] w-[130px]">About</p>
                         <div className="flex lg:flex-col gap-[18px] flex-wrap items-center lg:items-start">
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Menu</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Features</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">News & Blogs</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Help & Supports</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Menu</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Features</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">News & Blogs</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Help & Supports</a>
                         </div>
                     </div>
                     <div className="flex flex-col sm:flex-row lg:flex-col gap-y-4 gap-x-8 lg:gap-0 bottom-anim">
                         <p className="text-[26px] font-bold lg:pb-[56px] w-[130px] ">Company</p>
                         <div className="flex lg:flex-col gap-[18px] flex-wrap items-center lg:items-start ">
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">How we work</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Terms of service</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Pricing</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">FAQ</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">How we work</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Terms of service</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Pricing</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">FAQ</a>
                         </div>
                     </div>
                     <div className="flex flex-col sm:flex-row lg:flex-col  gap-y-4 gap-x-8 lg:gap-0 bottom-anim">
                         <p className="text-[26px] font-bold lg:pb-[56px]  w-[130px]  ">Contact&nbsp;Us</p>
                         <div className="lg:max-w-[332px] flex lg:flex-col gap-[18px] items-center lg:items-start flex-wrap ">
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">Akshya Nagar 1st Block 1st Cross</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">+1 234-567-8910</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">beanscene@mail.com</a>
-                            <a href="https://www.upwork.com/freelancers/~01a687c8f95b7913d7" target="_blank" rel="noopener noreferrer" className="footer-link">www.beanscene.com</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">Akshya Nagar 1st Block 1st Cross</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">+1 234-567-8910</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">beanscene@mail.com</a>
+                            <a href="mailto:volodymyr.kravets.dev@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-link">www.beanscene.com</a>
                         </div>
                     </div>
                 </div>
